55aec4ab7ac04175275ff936b15069f41db47bf5
OlszewskiMaciej <maciej.olszewski4@gmail.com>
2025-05-26 12:53:20 +0200

    Move SubscriptionStatus enum and ApiResponse trait for standardized API responses

--- app/Core/Enums/SubscriptionStatus.php
+++ app/Core/Enums/SubscriptionStatus.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Modules\Core\Enums;
+namespace App\Core\Enums;
 
 enum SubscriptionStatus: string
 {
--- app/Core/Traits/ApiResponse.php
+++ app/Core/Traits/ApiResponse.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Modules\Core\Traits;
+namespace App\Core\Traits;
 
 use Illuminate\Http\JsonResponse;
 
