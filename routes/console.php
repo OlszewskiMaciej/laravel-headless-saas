bf6453432944df7e25123334c0d1972db57b2bec
OlszewskiMaciej <maciej.olszewski4@gmail.com>
2025-07-20 16:14:32 +0200

    refactor: Remove unused 'inspire' command from console routes

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Log;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 // Check for expired trials and downgrade users
 Schedule::command('subscription:check-expired-trials')
